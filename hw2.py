e048ad60d793f5e70df809f89d4ee72ffbc6bdcd
Heather MacGregor <weathermacgregor@gmail.com>
2017-07-25 12:40:51 -0700

    Commit 5

--- hw2.py
+++ hw2.py
@@ -1,17 +1,13 @@
-f = open("prime.txt", "r+")
+f = open("C:/Users/macgr/Desktop/ahhhh/scream.txtscream.txt", "w+")
 lower = 1
 upper = 1000
 
 for x in range(lower,upper + 1):
-    if x > 2:
+    if x > 3:
         for i in range(2,x):
             if (x % i) == 0:
                 break
         else:
-            f.write(str(x))
+            f.write(str(x),)
 
 f.close()
-                
-
-
-    
